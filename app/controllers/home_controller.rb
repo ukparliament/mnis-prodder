e84f28b49b9d9ac2df990327fa4cc2d0d012ccc7
Michael Smethurst <michael@smethur.st>
2022-05-24 21:15:38 +0100

    redirect uses rails url helper

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,6 +31,9 @@ class HomeController < ApplicationController
     @include_parameters = assemble_include_parameters
     
     # We pass the API filter and include parameters we've constructed to the parse method. This method calls the public MNIS API with these parameters, parses the resulting XML and displays as HTML.
-    redirect_to( "/parse?filter=#{@filter_parameters}&include=#{@include_parameters}" )
+    #redirect_to( "/parse?filter=#{@filter_parameters}&include=#{@include_parameters}" )
+    
+    
+    redirect to( parse_url( :filter => @filter_parameters, :include => @include_parameters ) )
   end
 end
