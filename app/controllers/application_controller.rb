4d9bc0a3f6dd7c8e1ae2bb5e07420109d0d53016
fantasticlife <michael@smethur.st>
2025-07-24 20:15:03 +0100

    switched to styling via gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
-	before_action do
-    create_crumb_container
-  end
+
+  include LibraryDesign::Crumbs
 
   $SITE_TITLE = 'MNIS Prodder'
-
-  private
-
-   def create_crumb_container
-    @crumb = []
-  end
 end
