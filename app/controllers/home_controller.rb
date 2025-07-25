d83706dd2db50abf1a1b9773477f17e5a6e0a4ed
James Jefferies <james@shedcode.co.uk>
2025-07-25 17:29:31 +0100

    Remove debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -33,11 +33,6 @@ class HomeController < ApplicationController
     # We assemble the include parameters.
     @include_parameters = assemble_include_parameters
 
-    logger.info "Host: #{request.headers['Host']}"
-    logger.info "X-Forwarded-Host: #{request.headers['X-Forwarded-Host']}"
-    logger.info "X-Forwarded-Proto: #{request.headers['X-Forwarded-Proto']}"
-
-    
     # We pass the API filter and include parameters we've constructed to the parse method. This method calls the public MNIS API with these parameters, parses the resulting XML and displays as HTML.
     redirect_to( "#{parse_url}?filter=#{@filter_parameters}&include=#{@include_parameters}" )
   end
