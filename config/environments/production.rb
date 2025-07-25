3207e8c751c41aca219d8656dd6093f281a7eca8
James Jefferies <james@shedcode.co.uk>
2025-07-25 10:25:47 +0100

    Set default url option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,4 +58,5 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  routes.default_url_options[:host] ||= "api.parliament.uk"
 end
